ce0c3930ce64442b130f0ca006e97f5651c5471d
Rafael Cardoso <rafael_tom10@hotmail.com>
2019-02-13 21:59:47 -0200

    Alterações no GIT_COMMANDS.txt e aula07-desafio11.py funcionando

---
+++ aula07-desafio11.py
@@ -0,0 +1,9 @@
+width = float(input('Digite a largura: '))
+height = float(input('Digite a altura: '))
+
+area = width * height
+liters = area/2
+
+print('A quantidade de tinta para pintar uma area de {} metros quadrados é {} litros'.format(area, liters))
+
+
