4f05ad6d0f56ab26ac4c7d27720c4c379ce206d7
Ricardo Felix <ricardofelix_mont@outlook.com>
2019-02-12 22:24:44 -0200

    My first Commit

---
+++ if_else_statement.py
@@ -0,0 +1,5 @@
+x = 10
+if x <= 10:
+    print('Valor ok')
+else:
+    print('Valor não baixo')
---
+++ loop_for.py
@@ -0,0 +1,19 @@
+"""
+Utilizando for loop
+
+O laço for só pode ser uzado com iteraveis(lista, dicionarios, tuplas, string...)
+ex: 
+
+name = "Rafael"
+for c in name:
+    print(c)
+"""
+
+name = 'Rafael'
+
+for c in name:  # Percorre cada letra e printa
+    print(c)
+
+# O for loop funciona como o for each no java
+
+
