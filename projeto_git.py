19b7f367a3d1b30e4bb2128f8904a14318629766
Rafael Cardoso <rafael_tom10@hotmail.com>
2019-02-12 22:10:44 -0200

    Essa é uma tabuada

--- projeto_git.py
+++ projeto_git.py
@@ -1,1 +1,14 @@
-print('Essa é o segundo commit')
+number = int(input('Escreva um numero inteiro: '))
+
+print('1 + {} = {}'.format(number, number+1))
+print('2 + {} = {}'.format(number, number+2))
+print('3 + {} = {}'.format(number, number+3))
+print('4 + {} = {}'.format(number, number+4))
+print('5 + {} = {}'.format(number, number+5))
+print('6 + {} = {}'.format(number, number+6))
+print('7 + {} = {}'.format(number, number+7))
+print('8 + {} = {}'.format(number, number+8))
+print('9 + {} = {}'.format(number, number+9))
+print('10 + {} = {}'.format(number, number+10))
+
+
