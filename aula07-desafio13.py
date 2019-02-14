e7f18944e827a5d2ff10044ca1b351002342655e
Rafael Cardoso <rafael_tom10@hotmail.com>
2019-02-13 22:34:40 -0200

    Exercise salary increase aula07-desafio13.py

---
+++ aula07-desafio13.py
@@ -0,0 +1,8 @@
+salary = float(input('Digite o seu salario: '))
+
+salary_increase = salary * (15/100)
+
+new_salary = salary + salary_increase
+
+print('O almento no salario foi de {} e o seu novo salario é {}'.format(salary_increase, new_salary))
+
