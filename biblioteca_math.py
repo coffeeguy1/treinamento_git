5c3ac12f56d547fdc602709f001e1b5a5a24dff6
Rafael Cardoso <rafael_tom10@hotmail.com>
2019-02-14 22:54:54 -0200

    Three more files about module math ceil, sqrt, floor

---
+++ biblioteca_math.py
@@ -0,0 +1,9 @@
+import math
+
+num = int(input('Digite um numero: '))
+
+raiz = math.sqrt(num)
+
+print('O valor digitado foi {} e a sua raiz é {:.2f}'.format(num, raiz))
+
+
---
+++ biblioteca_math2.py
@@ -0,0 +1,6 @@
+from math import sqrt, ceil
+
+num = int(input('Digite um valor: '))
+raiz = sqrt(num)
+
+print('O valor digitado foi {} e sua raiz arredondada é {}'.format(num, ceil(raiz)))
